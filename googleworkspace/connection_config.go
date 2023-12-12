a0c3b9852f02bf6741ac2bea17eb09b69987669c
Madhushree Ray <madhushree@turbot.com>
2023-12-12 23:03:10 +0530

    Remove config schema

--- googleworkspace/connection_config.go
+++ googleworkspace/connection_config.go
@@ -2,29 +2,13 @@ package googleworkspace
 
 import (
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/schema"
 )
 
 type googleworkspaceConfig struct {
-	CredentialFile        *string `cty:"credential_file"`
-	Credentials           *string `cty:"credentials"`
-	ImpersonatedUserEmail *string `cty:"impersonated_user_email"`
-	TokenPath             *string `cty:"token_path"`
-}
-
-var ConfigSchema = map[string]*schema.Attribute{
-	"credential_file": {
-		Type: schema.TypeString,
-	},
-	"credentials": {
-		Type: schema.TypeString,
-	},
-	"impersonated_user_email": {
-		Type: schema.TypeString,
-	},
-	"token_path": {
-		Type: schema.TypeString,
-	},
+	CredentialFile        *string `hcl:"credential_file"`
+	Credentials           *string `hcl:"credentials"`
+	ImpersonatedUserEmail *string `hcl:"impersonated_user_email"`
+	TokenPath             *string `hcl:"token_path"`
 }
 
 func ConfigInstance() interface{} {
